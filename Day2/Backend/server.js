4611830c79877307a92eb836d22f66f134d36dd9
144bharat <144bharatgautam@gmail.com>
2024-07-30 23:46:03 +0530

    Add react app and created json response from nodejs

--- Day2/Backend/server.js
+++ Day2/Backend/server.js
@@ -3,8 +3,60 @@ dotenv.config()
 import express from "express";
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send("my data");
+const jokes = [
+    {
+        id: 1,
+        title: 'Joke 1',
+        content: 'this is Joke 1'
+    },
+    {
+        id: 2,
+        title: 'Joke 2',
+        content: 'this is Joke 2'
+    },
+    {
+        id: 3,
+        title: 'Joke 3',
+        content: 'this is Joke 3'
+    },
+    {
+        id: 4,
+        title: 'Joke 4',
+        content: 'this is Joke 4'
+    },
+    {
+        id: 5,
+        title: 'Joke 5',
+        content: 'this is Joke 5'
+    },
+    {
+        id: 6,
+        title: 'Joke 6',
+        content: 'this is Joke 6'
+    },
+    {
+        id: 7,
+        title: 'Joke 7',
+        content: 'this is Joke 7'
+    },
+    {
+        id: 8,
+        title: 'Joke 8',
+        content: 'this is Joke 8'
+    },
+    {
+        id: 9,
+        title: 'Joke 9',
+        content: 'this is Joke 9'
+    },
+    {
+        id: 10,
+        title: 'Joke 10',
+        content: 'this is Joke 10'
+    }
+]
+app.get('/jokes',(req,res) => {
+    res.json(jokes);
 })
 
 app.listen(process.env.PORT,()=>{
